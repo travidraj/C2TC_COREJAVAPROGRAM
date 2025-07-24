4260855a7e2b8c1f80441cbd947958b62533d645
ASUS <ASUS@LAPTOP-V81KBID8.mshome.net>
2025-07-24 14:12:17 +0530

    this is my 3 project

---
+++ C2TC_COREJAVAPROGRAM/src/day2class/UserName.java
@@ -0,0 +1,19 @@
+package day2class;
+import java.util.*;
+public class UserName {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		System.out.println("travid");
+		String n= input.nextLine();
+		System.out.println("Hello"+n);
+
+	}
+
+
+
+
+	}
+
+
