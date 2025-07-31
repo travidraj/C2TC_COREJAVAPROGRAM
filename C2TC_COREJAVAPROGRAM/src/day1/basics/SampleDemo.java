c783029f0763f52342da006b88faffb60f153f99
dell <dell@DESKTOP-TL19HTR>
2025-07-31 14:52:26 +0530

    update

--- C2TC_COREJAVAPROGRAM/src/day1/basics/SampleDemo.java
+++
@@ -1,9 +0,0 @@
-package day1.basics;
-
-public class SampleDemo {
-
-	public static void main(String[] args) {
-		System.out.println("welcome to jfs");
-	}
-
-}
--- C2TC_CoreJavaProgram/src/dayten/MultiDimArrayDemo.java
+++ C2TC_CoreJavaProgram/src/dayten/MultiDimArrayDemo.java
@@ -1,5 +1,12 @@
 package dayten;
 
 public class MultiDimArrayDemo {
+	public static void main(String[] args) {
 
+		int c[][] = { { 12, 45 }, { 10, 40 }, { 5, 7 } }; // new int[3][2];
+
+		System.out.println("No. of Rows in C array : " + c.length);
+
+		MLArray.printArray(c);
+	}
 }
