331465ba3943db174c6510c7e484b133059c1b3e
dell <dell@DESKTOP-TL19HTR>
2025-08-04 09:58:16 +0530

    synchronization

---
+++ C2TC_CoreJavaProgram/src/Day14/synchronization/InsufficientBalanceException.java
@@ -0,0 +1,12 @@
+package Day14.synchronization;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException() {
+		super("Insufficient balance in your account");
+	}
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+	}
+}
